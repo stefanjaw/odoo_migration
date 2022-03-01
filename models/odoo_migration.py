e3b6853a36e3aa1e259290253c06bd0173c14490
Stefan J <stefanj@avalantec.com>
2022-03-01 05:29:52 +0000

    updated

--- models/odoo_migration.py
+++ models/odoo_migration.py
@@ -40,6 +40,7 @@ class OdooMigration(models.Model):
             return False
     
     def get_records_id(self, url, db, login_id, pwd, model, search_filter):
+        _logging.info("    DEF43")
         header = { 'Content-Type': 'application/json', }
         
         payload1 = {
@@ -59,13 +60,56 @@ class OdooMigration(models.Model):
             },
             'id': random.randint(0, 1000000000),
         }
-        
+        _logging.info("    DEF6")
         response = requests.post(
             url,
             data= json.dumps(payload1).encode().decode('utf-8'),
             headers = header,
         )
+        _logging.info("    DEF69 response: %s", response)
         try:
-            return response.json()
+            records_lst = response.json().get('result')
+            _logging.info("    DEF71 records_lst: %s", records_lst)
+            if len(records_lst) == 0:
+                _logging.info("  NO RECORDS FOUND")
+                return False
+            return records_lst
+        except:
+            return False
+        
+    def export_data(self, url, db, login_id, pwd, model, ids, vars1):
+        _logging.info("    DEF81")
+        header = { 'Content-Type': 'application/json', }
+        
+        payload1 = {
+            'jsonrpc': '2.0',
+            'method': 'call',
+            'params': {
+                'service': 'object',
+                'method': 'execute',
+                'args': [
+                    db,
+                    login_id,
+                    pwd,
+                    model,
+                    'export_data',
+                    ids,
+                    vars1,
+                ],
+            },
+            'id': random.randint(0, 1000000000),
+        }
+        _logging.info("    DEF102 payload1: %s", payload1)
+        response = requests.post(
+            url,
+            data= json.dumps(payload1).encode().decode('utf-8'),
+            headers = header,
+        )
+        _logging.info("    DEF108 response: %s", response)
+        try:
+            records_array = response.json().get('result')
+            #{'datas': [['_....], ... ]
+            _logging.info("    DEF111 records_id: %s", records_array)
+            return records_array.get('datas')
         except:
             return False
--- models/res_partner.py
+++ models/res_partner.py
@@ -31,30 +31,81 @@ class ResPartnerInheritMigration(models.Model):
         login_data = OdooMigration.get_loging_id(self,
             url, data.get('db'), data.get('usr'), pwd,
         )
-        _logging.info("  login_data: %s", login_data)
         if login_data == False:
             _logging.info("ERROR: Login")
+            return
 
         login_id = login_data.get('result')
-        _logging.info("  login_id: %s", login_id)
+        
         #GET RECORDS ID
         _logging.info("  DEF25")
         search_filter = data.get('search_filter')
         if not search_filter:
             search_filter = []
-        
-        response = OdooMigration.get_records_id(
+
+        _logging.info("    DEF44")
+        records_lst = OdooMigration.get_records_id(
             # url, db, login_id, pwd, model, search_filter
             self, url, data.get('db'), login_id, pwd,self._name, search_filter,
         )
-        
-        records_id = response.get('result')
-        if len(records_id) == 0:
-            _logging.info("  NO RECORDS FOUND")
+        if records_lst == False:
             return
-        _logging.info("  57records_id: %s", records_id)
+        _logging.info("  DEF57 records_lst: %s", records_lst)
 
-        
+        #GET RECORDS DATA
+        _logging.info("  DEF56")
+        remote_data_array = OdooMigration.export_data(
+            #url, db, login_id, pwd, model, ids, vars1
+            self, url, data.get('db'), login_id, pwd, self._name,
+            records_lst, data.get('remote_vars'),
+        )
+        if remote_data_array == False:
+            return
+        #_logging.info("  66 remote_data_array: %s", remote_data_array)
+
+        #Check Data if exists in THIS instance
+        remote_vars = data.get('remote_vars')
+        local_vars = data.get('local_vars')
+        vars_len = len( remote_vars )
+        _logging.info("    vars_len: %s", vars_len)
+
+        _logging.info("72 INICIO DEL FOR remote_data_array=====")
+        for remote_record_lst in remote_data_array:
+            
+            #Identifica el Remote External ID
+            remote_record_id = remote_record_lst[0]
+            
+            #Obtiene el Local Record ID Objeto
+            local_record_id = self.env.ref( remote_record_id )  # Es el Objeto Local
+            _logging.info("    80 test: %s", local_record_id.get_external_id() )
+            STOP81
+            
+            #Si no lo encuentra, se debe crear el registro
+            if not local_record_id:
+                STOP74_REGISTRO_NO_ENCONTRADO
+                CREAR_UN_ARRAY_PARA_CREAR_REGISTRO_POSTERIORMENTE
+                
+            #recorre cada variable del registro y
+            #hace match el dato local con data.get('remote_vars')
+            _logging.info("84 INICIO DEL FOR=====")
+            for index, value in enumerate( data.get('remote_vars') ):
+                _logging.info("  86 Index: %s value: %s", index, value)
+                remote_var = remote_vars[index]
+                local_var = local_vars[index]
+                if remote_var == "id":
+                    _logging.info("  DEF_84 remote_var es ID, Continue")
+                    continue
+                remote_value = remote_record_lst[index]
+                _logging.info("    88 remote_value: %s", remote_value)
+                local_value = eval( "local_record_id.{0}".format( local_var ) )
+                _logging.info("    90 local_value: %s", local_value)
+                if remote_value == local_value:
+                    continue
+                else:
+                    _logging.info("  DIFERENCIAS: REMOTE: %s LOCAL: %s",
+                        remote_value, local_value)
+                    STOP_SE_DEBE_ACTUALIZAR_EL_REGISTRO
+
         return
         
         
