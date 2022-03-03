efd946cd743957b13121bdde84cc57639b2f76ec
Stefan J <stefanj@avalantec.com>
2022-03-03 20:28:26 +0000

    updated

--- models/.ipynb_checkpoints/res_partner-checkpoint.py
+++ models/.ipynb_checkpoints/res_partner-checkpoint.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
 from .odoo_migration import OdooMigration
 
 import base64
@@ -9,12 +8,222 @@ import base64
 import logging
 _logging = logging.getLogger(__name__)
 
+import xmlrpc.client
+
 class ResPartnerInheritMigration(models.Model):
     _inherit = 'res.partner'
 
+    
+    
     def migrate(self, data):
-        _logging.info("DEF_14 migrate")
+        _logging.info("DEF_19 migrate")
 
+        #payment_model = self.env['account.payment']
+        #PROBAR LLAMANDO MANUALMENTE EL action_validate_invoice_payment con el SELF
+        
+        payment_int = 32098    #ID del PAGO
+        account_move = 233835  #ID del Asiento Contable de la Factura
+        account_move = 233890  #ID del Asiento Contable relacionado al PAGO
+        
+        account_move_line = 643877 #Apunte Contable Debito 0, Credito 1.99 
+        
+        payment_id = self.env['account.payment'].browse( payment_int )
+        
+        new_context = {'lang': 'es_CR', 'tz': 'America/Costa_Rica', 'uid': 2, 'allowed_company_ids': [1], 'params': {'action': 276, 'cids': 1, 'id': 233835, 'menu_id': 335, 'model': 'account.move', 'view_type': 'form'}, 'dont_redirect_to_payments': True, 'active_model': 'account.move', 'active_id': 233835, 'active_ids': [233835]}
+        output = payment_id.with_context( new_context )
+        output.action_post()
+        
+        _logging.info("  DEF30 output: %s", output)
+        _logging.info("  DEF31 output._context: %s", output._context)
+        
+        invoice_id = self.env['account.move'].browse( invoice_int )
+        new_context = {'bin_size': True, 'lang': 'es_CR', 'tz': 'America/Costa_Rica', 'uid': 2, 'allowed_company_ids': [1], 'params': {'action': 276, 'cids': 1, 'id': 233835, 'menu_id': 335, 'model': 'account.move', 'view_type': 'form'}, 'default_move_type': 'out_invoice'}
+        output = invoice_id.with_context( new_context )._compute_authorized_transaction_ids()
+        _logging.info("  DEF37 output: %s", output)
+        
+        return
+        
+        
+        payment_model.action_validate_invoice_payment = "stuff"
+        _logging.info( "DEF25: %s", dir(payment_model.action_validate_invoice_payment)  )
+        
+        STOP29
+        ''' Opcion 1
+        payment_model = self.env['account.payment']
+        payment_id = payment_model.browse( payment_int )
+        
+        #_logging.info( "  DEF2 dir payment_id: %s", dir(payment_id) )
+        #STOP26
+        payment_id.invoice_origin = invoice_int
+        _logging.info( "  DEF26 result: %s", payment_id.action_post() )
+        return
+        '''
+        
+        
+        url1 = "https://avalantec-intecr1-stag1b-4314073.dev.odoo.com"
+        db1 = "avalantec-intecr1-stag1b-4314073"
+        username1 = "admin"
+        password1 = "0987654321!"
+        
+        common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url1))
+        _logging.info("DEF_19 common.version(): %s", common.version()) 
+        
+        uid = common.authenticate(db1, username1, password1, {})
+        _logging.info("DEF_29 uid: %s", uid ) 
+        
+        models_xml = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url1))
+        _logging.info("DEF_31 models: %s", models_xml ) 
+
+        
+        confirm_result = models_xml.execute_kw(db1, uid, password1,
+            'account.payment',
+            'action_validate_invoice_payment',
+            [payment_int],
+            {   
+                'invoice_id': invoice_int,
+                #"active_id": invoice_int,
+                #"active_ids":[invoice_int],
+                #"active_model": "account.move",
+                #"default_invoice_ids": [invoice_int],#[[4,invoice_id,False]],
+                #"journal_type":"sale",
+                #"lang":"en_US",
+                #"search_disable_custom_filters": True,
+                #"type": "out_invoice",
+                #"tz": False,
+                
+            },
+        )
+        
+        _logging.info("DEF45 confirm_result: %s", confirm_result)
+        return
+        
+        
+        
+        '''
+        register_payment_row = [
+            [payment_id],
+            {
+                "active_id": invoice_id,
+                "active_ids":[invoice_id],
+                "active_model": "account.move",
+                "default_invoice_ids": [invoice_id],#[[4,invoice_id,False]],
+                "journal_type":"sale",
+                "lang":"en_US",
+                "search_disable_custom_filters": True,
+                "type": "out_invoice",
+                "tz": False,
+                #"uid": 2,
+            }
+        ]
+        _logging.info("DEF52 register_payment_row: %s", register_payment_row )
+        '''
+        
+        '''
+        output = models_xml.execute_kw("avalantec-intecr1-stag1b-4314073", 2, "0987654321!")
+        _logging.info("DEF42 output: %s", output)
+        return
+    
+    
+        '''
+        
+        confirm_result = models_xml.execute_kw(db1, uid, password1,
+            'account.payment',
+            'action_validate_invoice_payment',
+            register_payment_row,
+        )
+        
+        _logging.info("DEF45 confirm_result: %s", confirm_result)
+
+        return
+    
+        
+    def invoiceRegisterPayment(self,register_payment_row):        
+        
+        return confirm_result
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        invoice_id = self.env['account.move'].browse( 233835 )
+        #_logging.info("DEF20 record_id: %s", dir(record_id) )
+        
+        payment_id = 32071
+        vals = [{'date': '2022-03-02', 'amount': 1.23, 'payment_type': 'inbound', 'partner_type': 'customer', 'ref': '00100001040000000424time1655', 'journal_id': 71, 'currency_id': 39, 'partner_id': False, 'partner_bank_id': 10, 'payment_method_id': 1, 'destination_account_id': 6, 'payment_token_id': False, 'team_id': 1}]
+        #payment_id = record_id.payment_id.create( vals ) # ========
+        #payment_id.action_post()
+        #payment_id.action_validate_invoice_payment
+        #payment_id.authorized_transaction_ids
+        #payment_id.transaction_ids()
+        '''
+        payment_id = self.env['account.payment']
+            .with_context(active_ids=[self.id],active_model='account.invoice')
+            .create({
+            'amount': self.residual,
+            'journal_id': journal_id.id,
+            'payment_method_id': payment_method_ids and payment_method_ids[0].id
+        })
+        payment_id.action_validate_invoice_payment()
+        '''
+        
+        
+        account_payment = self.env['account.payment']
+        payment_id = account_payment.with_context(active_ids=[invoice_id.id],active_model=['account.move'])\
+            .create({
+                'amount': 3.21,
+                'journal_id': 71,
+                'payment_method_id': 1,   #1 Manual inbound, 2 Manual outbound, 3 Electronic Inbound
+                'partner_id': invoice_id.partner_id.id,
+        })#.action_post()
+        payment_id.action_validate_invoice_payment() #NO FUNCIONO
+        #payment_id.reconciled_invoice_ids('XXXXXXX')
+        _logging.info("DEF31 payment_transaction: %s", payment_id )
+        
+        #logging.info("  DEF25 payment_id: %s", dir(payment_id))
+        #logging.info("  DEF26 payment_id: %s", payment_id)
+        return
+        
+        #_logging.info("DEF24 output: %s", dir(output))
+        _logging.info("DEF24 output: %s", output._name)
+        
+        #_logging.info("DEF20 record_id: %s", record_id.authorized_transaction_ids() )
+        return
+        
+        '''
+        #============================ _onchange_partner_id
+        record_id = self.env['account.move'].browse(192219)
+        _logging.info("  20 record_id: %s", record_id)
+        _logging.info("  21 Fecha Contable: %s", record_id.date)
+        _logging.info("  22 Fecha Vencimiento: %s", record_id.invoice_date_due)
+        _logging.info("  23 Plazo de Pago: %s", record_id.invoice_payment_term_id.name)
+        
+        record_id.button_draft()
+        record_id._onchange_partner_id()
+        record_id.action_post()
+        '''
+        '''
+        record_id.write({
+            'partner_id': 73332,
+        })
+        '''
+        
+        return
+        
+        
+        #============================
         try:
             pwd = base64.b64decode( data.get('pwd') ).decode('utf-8')
         except:
@@ -106,6 +315,8 @@ class ResPartnerInheritMigration(models.Model):
                         remote_value, local_value)
                     STOP_SE_DEBE_ACTUALIZAR_EL_REGISTRO
 
+            DETECTAR_SI_EXISTE_EL_EXTERNAL_ID_COMO_CAMPO_DE_UN_REGISTRO
+                    
         return
         
         
--- models/odoo_migration.py
+++ models/odoo_migration.py
@@ -8,6 +8,8 @@ import base64
 from odoo.http import request
 import requests
 
+import uuid
+
 import logging
 _logging = logging.getLogger(__name__)
 
@@ -15,7 +17,45 @@ class OdooMigration(models.Model):
     _name = 'odoo_migration'
     _description = 'odoo_migration.odoo_migration'
 
+    def _make_request(self, url, params=False):
+        ''' Make a request to proxy and handle the generic elements of the reponse (errors, new refresh token).
+        '''
+        payload = {
+            'jsonrpc': '2.0',
+            'method': 'call',
+            'params': params or {},
+            'id': uuid.uuid4().hex,
+        }
+
+        try:
+            response = requests.post(
+                url,
+                json=payload,
+                timeout=TIMEOUT,
+                headers={'content-type': 'application/json'},
+            )
+        except (ValueError, requests.exceptions.ConnectionError, requests.exceptions.MissingSchema, requests.exceptions.Timeout, requests.exceptions.HTTPError):
+            msg = ('The url that this service requested returned an error. The url it tried to contact was %s', url)
+            raise ValidationError( _( msg ) )
+
+        if 'error' in response:
+            message = _('The url that this service requested returned an error. The url it tried to contact was %s. %s', url, response['error']['message'])
+            if response['error']['code'] == 404:
+                message = _('The url that this service does not exist. The url it tried to contact was %s', url)
+            raise AccountEdiProxyError('connection_error', message)
+
+        proxy_error = response['result'].pop('proxy_error', False)
+        if proxy_error:
+            error_code = proxy_error['code']
+
+        return response['result']
+
+    
+    
+    
+    
     def get_loging_id(self, url, db, user, pwd):
+        STOP57
         header = { 'Content-Type': 'application/json', }
         
         payload1 = {
