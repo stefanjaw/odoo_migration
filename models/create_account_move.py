9ed15256710b7a35d53d05ff79a936c4ae783477
root <root@instance-staging1.c.staging-214517.internal>
2022-08-23 04:44:00 +0000

    updated

--- models/create_account_move.py
+++ models/create_account_move.py
@@ -28,7 +28,11 @@ class OdooMigration(models.Model):
         local_vars = params.get('local_vars') or []
         remote_model = params.get('remote_model') or False
         offset = params.get('offset') or 0
+        remote_line_vars = params.get('remote_line_vars') or []
 
+        if remote_model == False:
+            _logging.info(f"Error: Model Not Found\n================================")
+            return False
         _logging.info(f"DEF21 params: {move_ext_id} {remote_filter} {order} {limit} {company_int}")
 
         company_id = self.env['res.company'].browse( company_int )
@@ -44,8 +48,81 @@ class OdooMigration(models.Model):
         _logging.info(f"  Login ID: {login_id}" )
 
         #Contar los registros para obtener el offset
-        move_ints = self.get_records_id( remote_url, remote_db, login_id, remote_pwd, remote_model, remote_filter, offset, limit, order )
-        _logging.info(f"  DEF48 move_ints: {move_ints}")
+        remote_move_ints = self.get_records_id( remote_url, remote_db, login_id, remote_pwd, remote_model, remote_filter, offset, limit, order )
+        _logging.info(f"  DEF48 remote_move_ints: {remote_move_ints}")
 
+        remote_move_header_data = self.get_records_data( remote_url, remote_db, login_id, remote_pwd, 'account.move', remote_move_ints, remote_vars ) 
+        _logging.info(f"  DEF55 {remote_move_header_data}\n")
 
-        STOP21
+        xml_ids_to_create = set()
+        for record in remote_move_header_data.get('datas'):
+            _logging.info(f"DEF59 {record}\n")
+            local_move_header_data = self.convert_external_id_to_local( remote_vars, record )
+            header_data = local_move_header_data.get('record_array')
+            xml_ids_to_create = local_move_header_data.get('xml_id_to_create')
+            _logging.info(f"DEF60 \nheader_data: {header_data} xml_ids_to_create: {xml_ids_to_create}")
+            lines_ids_pos = remote_vars.index('invoice_line_ids.id')
+            _logging.info(f"DEF65 lines_ids_pos: {lines_ids_pos}")
+            line_ints = record[lines_ids_pos]
+            if type( line_ints ) == str: line_ints = [ int(line_ints) ]
+            _logging.info(f"DEF68 {type(line_ints)}")
+            _logging.info(f"DEF69 {line_ints}")
+            remote_move_lines_data = self.get_records_data( remote_url, remote_db, login_id, remote_pwd, 'account.move.line', line_ints, remote_line_vars )
+            _logging.info(f"DEF71 {remote_move_lines_data}")
+            lines_dict = []
+            for line_data in remote_move_lines_data.get('datas'):
+                local_move_lines_data = self.convert_external_id_to_local( remote_line_vars, line_data )
+                _logging.info(f"DEF73 {local_move_lines_data}")
+                line_data = local_move_lines_data.get( 'record_array' )
+                xml_ids_to_create.update( local_move_lines_data.get('xml_id_to_create') )
+                line_json = self.convert_array_to_json(remote_line_vars, line_data)
+                lines_dict.append( line_json   )
+            _logging.info( f"DEF80 line_json: {lines_dict}"  )
+            STOP81
+            _logging.info(f"DEF81 remote_move_lines_data: {remote_move_lines_data}")
+            _logging.info(f"DEF82 xml_ids_to_create: {xml_ids_to_create}")
+            _logging.info(f"DEF83 len(xml_ids_to_create: {len(xml_ids_to_create)}")
+            if len(xml_ids_to_create) > 0:
+                move_id = self.env['account.move'].create({
+                            'move_type': 'entry',
+                            'ref': "Error External IDs para: " + record[0],
+                        })
+                _logging.info(f"  DEF89 move_id: {move_id}")
+                move_id.button_cancel()
+
+                activity_type_id = self.env.ref('mail.mail_activity_data_todo')
+                summary = "Error: External IDs no encontrados para: " + record[0] 
+                note = "<b>External IDs:</b><br>{0}<br>".format( xml_ids_to_create )
+                move_id.activity_schedule(
+                    '', None, summary, note,
+                    **{ 'user_id': activity_type_id.default_user_id.id,
+                        'activity_type_id': activity_type_id.id }
+                )
+        STOP100
+        return
+
+    def convert_external_id_to_local(self, vars_array, record_array):
+        xml_id_to_create = set()
+        for pos in range(1, len(vars_array)):
+            if vars_array[pos][-3:] == "/id" and record_array[pos] != False:
+                try:
+                    record_array[pos] = self.env.ref( "aaaa" + record_array[pos] ).id
+                    vars_array[pos] = vars_array[pos][0:-3]
+                except:
+                    xml_id_to_create.add( record_array[pos] )
+                    record_array[pos] = False
+                    vars_array[pos] = vars_array[pos][0:-3]
+        
+        return {'record_array': record_array,
+                'xml_id_to_create': xml_id_to_create,
+               }
+
+    def convert_array_to_json(self, vars_array, record_array):
+        _logging.info(f"DEF120 {record_array}")
+        output_json = {}
+        for pos in range(0, len(vars_array)):
+            _logging.info(f"DEF121 {vars_array[pos]}: {record_array[pos]}")
+            output_json[vars_array[pos]] = record_array[pos]
+            _logging.info(f"DEF123 output_json: {output_json}")
+
+        return output_json
