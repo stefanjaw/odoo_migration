7eea82ee522ec54be4aab9978dfbe0be4a381412
root <root@instance-staging1.c.staging-214517.internal>
2022-08-29 21:59:18 +0000

    updated

--- models/__init__.py
+++ models/__init__.py
@@ -6,4 +6,6 @@ from . import res_partner
 from . import res_company
 from . import create_account_move
 from . import create_res_partner_emails_extra
+from . import create_res_partner_fe_otros
+
 #from . import create_ir_attachment
