37f9fe0889f1f38a59264a0bbac9333e6bfc2010
root <root@instance-staging1.c.staging-214517.internal>
2022-09-02 00:52:05 +0000

    push

--- models/__init__.py
+++ models/__init__.py
@@ -8,6 +8,7 @@ from . import create_account_move
 from . import create_ir_exports
 from . import create_res_partner
 from . import create_res_partner_emails_extra
+from . import create_res_partner_image
 from . import create_res_partner_fe_otros
 
 #from . import create_ir_attachment
